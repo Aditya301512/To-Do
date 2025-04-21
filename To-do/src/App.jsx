eeed2b0742484423b34c909c15f54d7a85441f64
Ratish Kapoor <ratishkapoor5@gmail.com>
2025-04-21 14:06:23 +0530

    auth

--- To-do/src/App.jsx
+++ To-do/src/App.jsx
@@ -3,7 +3,9 @@ import { Provider, useDispatch, useSelector } from "react-redux";
 import { store } from "./redux/store";
 import { addTask, removeTask, loadTasks } from "./redux/taskSlice";
 import { login, logout } from "./redux/authSlice";
+import authReducer from "./redux/authSlice";
 import taskReducer from './redux/taskSlice'
+import { Provider } from "react-redux";
 import "./App.css";
 
 function TodoApp() {
